0cfdf41edadd61301d2f4f496823b7008acee7d0
lotanh <lotan3600@gmail.com>
2021-04-19 23:34:44 +0300

    New project, building my portfolio

---
+++ week3/dailychallenge/1/index.js
@@ -0,0 +1,25 @@
+// delete the bananna
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.shift();
+console.log(fruits);
+// Sort the array in alphabetical order.
+fruits.sort();
+console.log(fruits);
+// Add kiwi to the end of the array
+fruits.push("Kiwi");
+console.log(fruits);
+// deleting Apple
+fruits.splice(0,1)
+console.log(fruits);
+// sort in reverse
+fruits.reverse();
+console.log(fruits);
+// done
+
+
+
+
+
+
+
+
---
+++ week3/dailychallenge/2/index.js
@@ -0,0 +1,3 @@
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// access oranges
+console.log([1][1][0]);
