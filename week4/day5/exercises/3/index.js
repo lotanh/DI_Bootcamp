c09144eb19dcd95dbb3eaba0e6f59fbf24dc5081
lotanh <lotan3600@gmail.com>
2021-05-04 15:59:16 +0300

    Exercises on DOM

---
+++ week4/day5/exercises/3/index.js
@@ -0,0 +1,7 @@
+let div = document.querySelector('div')
+div.style.padding = "30px";
+div.style.backgroundColor = "lightblue";
+let ul = document.querySelector('ul')
+ul.firstElementChild.style.display = 'none'
+ul.lastElementChild.style.border = 'solid'
+document.body.style.fontSize = "30px"
