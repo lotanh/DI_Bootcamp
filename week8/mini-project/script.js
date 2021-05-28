40926d7f475edae77b336cd25abdb80ad11a87ca
lotanh <lotan3600@gmail.com>
2021-05-28 22:01:05 +0300

    Added a condition  if the search bar is empty

--- week8/mini-project/script.js
+++ week8/mini-project/script.js
@@ -4,35 +4,38 @@ let currentWeather =[]
 // getting the info of the weather from the user input 
 
 const checkWeather = () => {
+    let userInput = document.getElementById('inp').value;
 
-    let x = new XMLHttpRequest();
+    if (userInput == '') {
+        alert('Please write a city!')
 
-    let cityName = document.getElementById('inp').value
-
-    cityName = cityName.toLowerCase()
-
-    console.log(cityName)
-
-    x.open('get' ,`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=6bc236fa8bd5e7e03f83fd8cea3eac74`)
+    } else {
+        userInput = userInput.toLowerCase();
+        console.log(userInput);
     
-    x.send()
+        let x = new XMLHttpRequest();
     
-    x.onload = function() {
-        if(x.status != 200){
-            console.log(x.status);
-        }
-        else{
-            var weatherData = JSON.parse(x.response)
-            console.log(weatherData)
-            currentWeather.push(weatherData)
-            console.log(currentWeather)
-        }
-    }  
+        x.open('get' ,`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=6bc236fa8bd5e7e03f83fd8cea3eac74`);
+        
+        x.send();
+        
+        x.onload = function() {
+            if(x.status != 200){
+                console.log(x.status);
+            }
+            else{
+                var weatherData = JSON.parse(x.response)
+                console.log(weatherData)
+                currentWeather.push(weatherData)
+                console.log(currentWeather)
+            }
+        }  
+    } 
 }
 
 
 const cityBox = (city) => {
     let div = document.createElement('div')
-    let city = document.createElement('p')
-    let country = document.createElement('p')
+    let cityName = document.createElement('p')
+    let countryName = document.createElement('p')
 }
