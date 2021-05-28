6cdb1c1ffd0d92dedbc8dccf6db128dd24fc1d82
lotanh <lotan3600@gmail.com>
2021-05-29 00:18:18 +0300

    Added the API data and created a function that takes the data and create a div with a delete button

--- week8/mini-project/script.js
+++ week8/mini-project/script.js
@@ -24,18 +24,61 @@ const checkWeather = () => {
                 console.log(x.status);
             }
             else{
+                userInput.value = ""
                 var weatherData = JSON.parse(x.response)
                 console.log(weatherData)
                 currentWeather.push(weatherData)
-                console.log(currentWeather)
+                // console.log(currentWeather)
+                createCityCard(weatherData)
             }
         }  
     } 
 }
 
 
-const cityBox = (city) => {
+const createCityCard = (city) => {
+
+    // creating element
+
+    let container = document.getElementById('container')
     let div = document.createElement('div')
+    div.classList.add('city-div')
     let cityName = document.createElement('p')
-    let countryName = document.createElement('p')
+    cityName.classList.add('city-p')
+    let icon = document.createElement('img')
+    icon.classList.add('weather-icon')
+    let temp = document.createElement('p')
+    temp.classList.add('temp')
+    let tempStatus = document.createElement('p')
+    let deleteCity = document.createElement('button')
+    deleteCity.classList.add('delete')
+
+    // adding value to them
+
+    cityName.innerText = `${city.name}, ${city.sys.country}`
+    let iconWeather = city.weather[0].icon
+    icon.src = `http://openweathermap.org/img/wn/${iconWeather}@2x.png`
+    // console.log(icon)
+    let tempK = city.main.temp
+    let tempC = Math.round(tempK - 273.15)
+    temp.innerText = tempC + "°"
+    tempStatus.innerText = city.weather[0].description
+    deleteCity.innerText = 'X'
+
+
+    // appending them
+
+    div.appendChild(cityName)
+    div.appendChild(icon)
+    div.appendChild(temp)
+    div.appendChild(tempStatus)
+    div.appendChild(deleteCity)
+    container.appendChild(div)
+
+    // delete div
+
+    deleteCity.addEventListener('click', () => {
+        div.style.display = 'NONE'
+    })
 }
+
