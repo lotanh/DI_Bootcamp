f89649f2b9bf09f9f0ddb153dffc18b85178bfb2
lotanh <lotan3600@gmail.com>
2021-06-22 23:22:27 +0300

    First steps in Node js

---
+++ week12/monday/exercises/npm-node/main.js
@@ -0,0 +1,13 @@
+// let a = 5;
+// let b = 10;
+// console.log(`Hello you are ${a+b}`);
+// console.log('Hi, thanks for running me with npm start!')
+
+let a = 5;
+let b = 10;
+let x;
+let c = 6;
+x = a + b;
+console.log(x)
+let A = 10 * 5 * 20
+console.log(A);
